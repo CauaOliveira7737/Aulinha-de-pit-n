4e89404ec9c0827bdd739bcf438da85f8e56a68f
Carlos Henrique <c.henrique.silva1103@outlook.com>
2025-03-19 15:27:35 -0300

    Update app.py
    Feito com carinho pelo brother

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-nome = "Ban"
-idade = 5584
-poder = " INVENCÍVEL"
+nome = input("Qual é o nome do(a) personagem? ")
+idade = input("Qual a idade do seu personagem? ")
+poder = input("Qual é o poder do seu personagem? ")
 
-print(f"Ban com seu poder{poder} nunca morreu, e tem {idade} anos")
+print(f"{nome} com seu poder{poder} nunca morreu, e tem {idade} anos")
+print(f"Nenhum ser é consegue derrotar o {nome}")
+print(f"A não ser seu maior terror.. O Haddad com seu poder de taxação!!!")
